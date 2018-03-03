72521e813827498a237fc962e2c01815bd529ebb
xsthunder <xsthunder@outlook.com>
2018-03-03 12:42:05 +0800

    add update pay

--- app/router/person.js
+++ app/router/person.js
@@ -4,13 +4,32 @@
  */
 "use strict";
 const Router = require('koa-router');
-
 const router = Router();
 
 router.get('/',async(ctx)=>{
     let Person = ctx.mongoose.model('Person');
     try{
         let ans = await Person.find().exec();
+        ctx.good(null,ans);
+    }catch (err){
+        ctx.bad(null,err);
+    }
+});
+
+router.post('/',async(ctx)=>{
+    let body = ctx.request.body.fields;
+
+    if(body['asdfpv[12vblka;1'] !== 'asdf[v]wqael,23.,vf')return ctx.bad('not allowed');
+    if(!body['who'])return ctx.bad('missing field');
+    let Person = ctx.mongoose.model('Person');
+    try{
+        // console.log(body['who']);
+        let ans = await Person.findOne({
+            who:body['who']
+        }).exec();
+        // console.log(ans);
+        if(!ans)return ctx.bad(`deal with name ${body['who']} not exits`);
+        ans = await ans.pay().save();
         ctx.good(null,ans);
     }catch (err){
         ctx.bad(null,err);
